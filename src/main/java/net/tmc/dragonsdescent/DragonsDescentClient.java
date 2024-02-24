67b93b7436c3ce4e17b2db6b71db7a13f9d9de1d
Alfred <AlfredTheKingOfSpace@gmail.com>
2024-02-23 22:42:59 -0500

    Workspace and Mod Setup
    Used https://youtu.be/0Pr_iHlVKsI?si=Wr4aKrzC6rKeGfd1 as a tutorial, using intelliJ because the gods hate me

---
+++ src/main/java/net/tmc/dragonsdescent/DragonsDescentClient.java
@@ -0,0 +1,10 @@
+package net.tmc.dragonsdescent;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DragonsDescentClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
